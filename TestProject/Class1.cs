0b578a29d151cd731cf3b224284b31b896ba64f1
Your Name <alexei.reb@gmail.com>
2012-03-28 22:12:32 +0300

    Added Test Class

---
+++ TestProject/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+namespace TestProject
+{
+    public class Class1
+    {
+    }
+}
